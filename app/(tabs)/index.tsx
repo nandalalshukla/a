7d6fadce059eea133ee4b4fccb644729f8dd3c8b
Nandalal Shukla <shuklanandalal@gmail.com>
2026-04-12 15:08:20 +0530

    added fonts

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,9 +1,13 @@
 import { Text, View } from "react-native";
-
+import { SafeAreaView } from "react-native-safe-area-context";
 export default function App() {
   return (
-    <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-xl font-bold text-red-700 ">Welcome to Nativewind!</Text>
-    </View>
+    <SafeAreaView className="bg-background flex-1 p-5">
+
+        <Text className="text-xl font-bold text-red-700 ">
+          Welcome to Nativewind!
+        </Text>
+
+    </SafeAreaView>
   );
 }
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,23 @@
-import { Stack } from "expo-router";
+import { useFonts } from "expo-font";
+import { SplashScreen, Stack } from "expo-router";
+import { useEffect } from "react";
 import "../global.css";
 
 export default function RootLayout() {
+  const [fontsLoaded] = useFonts({
+    "sans-regular": require("../assets/fonts/PlusJakartaSans-Regular.ttf"),
+    "sans-medium": require("../assets/fonts/PlusJakartaSans-Medium.ttf"),
+    "sans-semibold": require("../assets/fonts/PlusJakartaSans-SemiBold.ttf"),
+    "sans-bold": require("../assets/fonts/PlusJakartaSans-Bold.ttf"),
+  });
+
+  useEffect(() => {
+    if (fontsLoaded) {
+      SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+  if (!fontsLoaded) {
+    return null;
+  }
   return <Stack />;
 }
